4baf55622ebce4895675b33af330e84ca189e80e
AlfredSM <ingalfresm@gmail.com>
2022-10-15 18:25:37 -0500

    segunda pre entrega

--- JS/main.js
+++ JS/main.js
@@ -1,38 +1,79 @@
-const userName = prompt("Ingresa tu nombre");
-let userAge = parseInt(prompt("Ingresa tu edad"));
+//const userName = prompt("Ingresa tu nombre");
+//let userAge = parseInt(prompt("Ingresa tu edad"));
+const carrito = []
+const stock = [
+  { id: 1, name: "glicerina", price: 60 },
+  { id: 2, name: "caléndula", price: 50 },
+  { id: 3, name: "avena", pice: 70 },
+  { id: 4, name: "café", price: 70 }
+];
+
 
 function welcome(a, b) {
     if (b >= 18) {
-        alert(a + ", ¡bienvenid@ a Jabones Shaddai!");
-    } else {
-        alert(a + ", antes de comprar pide permiso a tus padres.")
+        Swal.fire({
+          title: `¡Bienvenid@ ${a}!`,
+          text: 'Disfruta tu visita',
+            icon: 'success',
+            confirmButtonText: 'Avanzar'
+          })
+          // alert(a + ", ¡bienvenid@ a Jabones Shaddai!");
+        } else {
+          Swal.fire({
+        title: `¡Bienvenid@ ${a}!`,
+        text: 'Antes de comprar pide permiso a tus padres',
+        icon: 'info',
+        confirmButtonText: 'Avanzar'
+      })
     }
-}
+  }
+  
+  //welcome(userName, userAge)
 
-welcome(userName, userAge)
-
-function comprar() {
-    let products=parseInt(prompt("¿Cuántos productos deseas comprar?"));
-    let options = ""
-    for (i = 1; i <= products; i++) {
-        alert("Introduce la opción deseada para añadir a carrito");
-        options = prompt("a) Jabón para manos \nb) Jabón para cara \nc) Jabón corporal");
-        switch(options){
-            case("a"):
-            alert("Agregaste jabón para manos");
-            break;
-            case("b"):
-            alert("Agregaste jabón para cara");
-            break;
-            case("c"):
-            alert("Agregaste jabón corporal");
-            break;
+  let seguir 
+  let select
+  let total= 0
+  
+  
+  function agregarACarrito(carrito){
+    do {
+      select = parseInt(prompt("Ingresa la opción que quieres agregar a tu carrito" + "\n"
+      + "1. Jabón de glicerina (60 MXN)" + "\n" 
+      + "2. Jabón de caléndula (50 MXN)" + "\n"
+      + "3. Jabón de avena (70 MXN)" + "\n" 
+      + "4. Jabón de café (70MXN)"))
+      seguir=prompt("¿quieres añadir algo más?")
+    switch(select){
+      case 1:
+        select=stock.find((el) => el.name == "glicerina")
+        carrito.push(select)
+        total += 60
+        break
+      case 2:
+        select=stock.find((el) => el.name == "caléndula")
+        carrito.push(select)
+        total += 50
+        break
+        case 3:
+          select=stock.find((el) => el.name == "avena")
+          carrito.push(select)
+          total += 70
+          break
+          case 4:
+            select=stock.find((el) => el.name == "café")
+            carrito.push(select)
+            total +=70
+            break
             default:
-            alert("Selecciona una opción adecuada");
-        }
-    }
-    alert("En tu carrito hay " + products + " productos.");
-}
+              alert("ingresa una opción válida")
+            }
+          } while(seguir=="si")
+  }
+  agregarACarrito(carrito)
+  console.log(carrito)
 
-comprar();
-
+  carrito.forEach((item) => {
+    alert(`Tienes en tu carrito: ${item.name}`)
+    alert("tu total es de " + total)
+  })
+  
